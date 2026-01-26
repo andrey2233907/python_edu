06acf4344551266a7a8da5e0f95f6667803d5960
andrey <279014643@ACADEMY.TOP>
2026-01-26 17:44:49 +0300

    sys, os, datetime, argparse

--- main_edu.py
+++ main_edu.py
@@ -30,7 +30,11 @@ df = pd.read_csv('data.csv').fillna('Underfined')
 # sorted = df.groupby('protocol').count()['timestamp']
 # print(sorted)
 
+
+
 # MATPLOTLIB
+
+
 
 import matplotlib.pyplot as plt
 # df = pd.read_csv('data.csv')
@@ -38,7 +42,7 @@ import matplotlib.pyplot as plt
 
 # x = list(range(-10, 11))
 # y = [i**2 for i in x]
-plt.style.use('dark_background')
+# plt.style.use('dark_background')
 # plt.plot(x, y)
 # plt.pie([1, 2, 3, 4, 5])
 # plt.legend('')
@@ -48,14 +52,65 @@ plt.style.use('dark_background')
 # plt.show()
 
 
-df = pd.read_csv('ips.csv')
+# df = pd.read_csv('ips.csv')
 # plt.pie(df['data_transfered'].to_list(), colors=["#E2E2E2"])
 # plt.legend(df['ip'].to_list())
 
 # plt.show()
 
-legend = df['ip']
-values = df['data_transfered']
-plt.xticks(rotation=45)
-plt.bar(legend, values, 1)
-plt.show()
+# legend = df['ip']
+# values = df['data_transfered']
+# plt.xticks(rotation=45)
+# plt.bar(legend, values, 1)
+# plt.show()
+
+
+
+# SYS, OS, DATETIME, ARGPARSE
+
+import sys
+import os
+import argparse
+from datetime import datetime
+# print(sys.argv)
+
+
+
+# path = sys.argv[1]
+# if not os.path.exists(path):
+#     print(f'Файла по такому пути не существует')
+#     exit()
+# if not os.access(path, os.R_OK):
+#     print(f'Нет доступа на чтение')
+#     exit()
+# with open(path) as file:
+#     for line in file:
+#         print(f'{line.strip()}')
+
+# dirname = sys.argv[1] if len(sys.argv) > 1 else 'new dir'
+# os.mkdir(dirname)
+
+
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument('output_path', help='Путь к файлу для записи')
+# parser.add_argument('-p', '--path', help='путь к файлу для чтения')
+# parser.add_argument('-r', '--register', help='Переводит все строки файла в верхний регистр', action='store_true', required=False)
+
+# args = parser.parse_args()
+# if not os.path.exists(parser.path):
+#     print('Файла не существует')
+#     exit()
+
+# with open(args.path, encode='utf-8') as file:
+#     for line in file:
+#         if args.register:
+#             print(line.upper().strip())
+#         else:
+#             print(line.strip())
+        
+
+
+# print(datetime.now())
+
+
